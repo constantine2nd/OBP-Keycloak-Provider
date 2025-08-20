cae23b23e4b9c4c4343eca3f6b0ae47e903a94cc
Marko Milić <marko@tesobe.com>
2025-08-20 12:52:38 +0200

    feature/Setup default configurable properties that appear in the Keycloak Admin Console

--- src/main/java/io/tesobe/providers/KcUserStorageProviderFactory.java
+++ src/main/java/io/tesobe/providers/KcUserStorageProviderFactory.java
@@ -1,6 +1,7 @@
 package io.tesobe.providers;
 
 import io.tesobe.config.DatabaseConfig;
+import java.util.List;
 import org.jboss.logging.Logger;
 import org.keycloak.component.ComponentModel;
 import org.keycloak.models.KeycloakSession;
@@ -9,22 +10,28 @@ import org.keycloak.provider.ProviderConfigProperty;
 import org.keycloak.provider.ProviderConfigurationBuilder;
 import org.keycloak.storage.UserStorageProviderFactory;
 
-import java.util.List;
-
-public class KcUserStorageProviderFactory implements UserStorageProviderFactory<KcUserStorageProvider> {
+public class KcUserStorageProviderFactory
+    implements UserStorageProviderFactory<KcUserStorageProvider> {
 
     public static final String PROVIDER_ID = "obp-keycloak-provider";
 
-    private static final Logger log = Logger.getLogger( KcUserStorageProviderFactory.class );
+    private static final Logger log = Logger.getLogger(
+        KcUserStorageProviderFactory.class
+    );
     private static volatile boolean configurationValidated = false;
 
     @Override
-    public KcUserStorageProvider create(KeycloakSession session, ComponentModel model) {
+    public KcUserStorageProvider create(
+        KeycloakSession session,
+        ComponentModel model
+    ) {
         // Validate configuration on first provider creation
         if (!configurationValidated) {
             synchronized (this) {
                 if (!configurationValidated) {
-                    log.info("Validating runtime database configuration for OBP Keycloak Provider");
+                    log.info(
+                        "Validating runtime database configuration for OBP Keycloak Provider"
+                    );
                     DatabaseConfig.validateConfiguration();
                     configurationValidated = true;
                 }
@@ -47,62 +54,64 @@ public class KcUserStorageProviderFactory implements UserStorageProviderFactory<
     @Override
     public List<ProviderConfigProperty> getConfigProperties() {
         return ProviderConfigurationBuilder.create()
-                .property()
-                .name("priority")
-                .type(ProviderConfigProperty.STRING_TYPE)
-                .label("Priority")
-                .defaultValue("0")
-                .helpText("Priority of this provider when looking up users. Lower values have higher priority.")
-                .add()
-                .property()
-                .name("enabled")
-                .type(ProviderConfigProperty.BOOLEAN_TYPE)
-                .label("Enabled")
-                .defaultValue("true")
-                .helpText("Set to true to enable this user storage provider")
-                .add()
-                .property()
-                .name("cachePolicy")
-                .type(ProviderConfigProperty.STRING_TYPE)
-                .label("Cache Policy")
-                .defaultValue("DEFAULT")
-                .helpText("Cache policy for this provider")
-                .add()
-                .property()
-                .name("importEnabled")
-                .type(ProviderConfigProperty.BOOLEAN_TYPE)
-                .label("Import Users")
-                .defaultValue("true")
-                .helpText("Enable importing users from external storage to Keycloak database")
-                .add()
-                .property()
-                .name("syncRegistrations")
-                .type(ProviderConfigProperty.BOOLEAN_TYPE)
-                .label("Sync Registrations")
-                .defaultValue("true")
-                .helpText("Should newly created users be created in this external storage")
-                .add()
-                .property()
-                .name("allowUserProfileEdit")
-                .type(ProviderConfigProperty.BOOLEAN_TYPE)
-                .label("Allow User Profile Editing")
-                .defaultValue("true")
-                .helpText("Allow users to edit their own profile information")
-                .add()
-                .property()
-                .name("editMode")
-                .type(ProviderConfigProperty.LIST_TYPE)
-                .label("Edit Mode")
-                .options("READ_ONLY", "WRITABLE", "UNSYNCED")
-                .defaultValue("WRITABLE")
-                .helpText("Set to WRITABLE to allow user profile updates")
-                .add()
-                .build();
+            .property()
+            .name("priority")
+            .type(ProviderConfigProperty.STRING_TYPE)
+            .label("Priority")
+            .defaultValue("0")
+            .helpText(
+                "Priority of this provider when looking up users. Lower values have higher priority."
+            )
+            .add()
+            .property()
+            .name("enabled")
+            .type(ProviderConfigProperty.BOOLEAN_TYPE)
+            .label("Enabled")
+            .defaultValue("true")
+            .helpText("Set to true to enable this user storage provider")
+            .add()
+            .property()
+            .name("cachePolicy")
+            .type(ProviderConfigProperty.STRING_TYPE)
+            .label("Cache Policy")
+            .defaultValue("DEFAULT")
+            .helpText("Cache policy for this provider")
+            .add()
+            .property()
+            .name("importEnabled")
+            .type(ProviderConfigProperty.BOOLEAN_TYPE)
+            .label("Import Users")
+            .defaultValue("false")
+            .helpText(
+                "Enable importing users from external storage to Keycloak database"
+            )
+            .add()
+            .property()
+            .name("syncRegistrations")
+            .type(ProviderConfigProperty.BOOLEAN_TYPE)
+            .label("Sync Registrations")
+            .defaultValue("false")
+            .helpText(
+                "Should newly created users be created in this external storage"
+            )
+            .add()
+            .property()
+            .name("allowUserProfileEdit")
+            .type(ProviderConfigProperty.BOOLEAN_TYPE)
+            .label("Allow User Profile Editing")
+            .defaultValue("false")
+            .helpText("Allow users to edit their own profile information")
+            .add()
+            .property()
+            .name("editMode")
+            .type(ProviderConfigProperty.LIST_TYPE)
+            .label("Edit Mode")
+            .options("READ_ONLY", "WRITABLE", "UNSYNCED")
+            .defaultValue("READ_ONLY")
+            .helpText("Set to WRITABLE to allow user profile updates")
+            .add()
+            .build();
     }
-
-
-
-
 
     @Override
     public void close() {
